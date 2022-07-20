e2cabb11c1f5804f8b5d14d7e7ba965362183162
renzoregio <renzorregio@gmail.com>
2022-07-20 17:22:36 +0800

    POC

--- server.js
+++ server.js
@@ -1,15 +1,53 @@
 import { ApolloServer, gql } from "apollo-server"
 
+let tempMovies = [
+    {
+        title: "First Movie",
+        year: 1997
+    },
+    {
+        title: "Second Movie",
+        year: 2021
+    },
+    {
+        title: "Third Movie",
+        year: 2018
+    }
+]
+
 const typeDefs = gql`
+
+   type Movie {
+    title: String
+    year: Int
+   } 
+
     type Query {
-        hello : String
+        movies : [Movie]
+        movie: Movie
+    }
+
+    type Mutation {
+        createMovie(title: String!): Boolean
+        deleteMovie(title: String!): Boolean
     }
 `
 
 const resolvers = {
     Query: {
-        hello: () => {
-            return "Hello There";
+        movies: () => tempMovies,
+        movie: () => ({ title: "Hello", year: 2021 })
+    },
+    Mutation: {
+        createMovie: (_, { title }) => {
+            const movie = { title: title, year: 2022 }
+            tempMovies.push(movie)
+            return true
+        },
+        deleteMovie: (_, { title }) => {
+            const originalLength = tempMovies.length;
+            tempMovies = tempMovies.filter((movie) => movie.title !== title)
+            return tempMovies.length !== originalLength ? true : false;
         }
     }
 }
