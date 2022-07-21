39e2e1d832bf0a6d79a497bbf78869289775c3c1
renzoregio <renzorregio@gmail.com>
2022-07-21 20:57:02 +0800

    Deleted POC - Starting Instaclone Project

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 import { ApolloServer, gql } from "apollo-server"
 import schema from "./schema"
 
 const server = new ApolloServer({ schema });
+const PORT = process.env.PORT;
 
-server.listen().then(() => console.log("Server is running on http://localhost:4000"))
+server.listen(PORT).then(() => console.log(`Server is running on http://localhost:${PORT}/`))
