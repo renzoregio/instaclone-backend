baf22c438618ba4418840368e8cda51d7a158cf6
renzoregio <renzorregio@gmail.com>
2022-07-22 15:23:29 +0800

    Add seeProfile query function

--- users/users.queries.js
+++ users/users.queries.js
@@ -1,4 +1,23 @@
+import { client } from "../client"
+
 export default {
     Query: {
+        seeProfile: async (_, { userName }) => {
+            try {
+                const user = await client.user.findUnique({
+                    where: {
+                        userName
+                    }
+                })
+
+                if (!user) {
+                    throw new Error("User not found.")
+                }
+
+                return user;
+            } catch (err) {
+                return err;
+            }
+        }
     }
 }
