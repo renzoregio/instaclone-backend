e410572197bd52c7df6b5a9603da515fa7f86132
renzoregio <renzorregio@gmail.com>
2022-07-23 11:10:47 +0800

    Added editProfile mutation function

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,7 +1,41 @@
+import { client } from "../../client"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+
 export default {
     Mutation: {
-        editProfile: (_, { firstName, lastName, username, email, password }) => {
-            console.log("HI")
+        editProfile: async (_, { firstName, lastName, userName, email, password: newPassword, token }) => {
+            try {
+                const { id } = jwt.verify(token, process.env.SECRET_KEY)
+
+                if (!id) {
+                    throw new Error("Could not update profile.")
+                }
+
+                let hashedPassword = null;
+
+                if (newPassword) {
+                    hashedPassword = await bcrypt.hash(newPassword, 10)
+                }
+
+                const updatedUser = await client.user.update({
+                    where: {
+                        id
+                    },
+                    data: {
+                        firstName, lastName, userName, email,
+                        ...(hashedPassword && { password: hashedPassword })
+                    }
+                })
+
+                if (!updatedUser.id) {
+                    throw new Error("Could not update profile.")
+                }
+
+                return { ok: true }
+            } catch (err) {
+                return { ok: false, error: err.message }
+            }
         }
     }
 }
--- users/editProfile/editProfile.typeDefs.js
+++ users/editProfile/editProfile.typeDefs.js
@@ -8,7 +8,7 @@ export default gql`
     }
 
     type Mutation{
-        editProfile(firstName: String, lastName: String, userName: String, email: String, password: String): EditProfileResult!
+        editProfile(firstName: String, lastName: String, userName: String, email: String, password: String, token: String!): EditProfileResult!
     }
 
 `
