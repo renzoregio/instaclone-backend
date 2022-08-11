55f843b8ae5e35805ceb97c1c1d25147d6154aa7
renzoregio <renzorregio@gmail.com>
2022-08-11 10:43:50 +0800

    Filter subscription by roomId

--- src/messages/roomUpdates/roomUpdates.resolvers.ts
+++ src/messages/roomUpdates/roomUpdates.resolvers.ts
@@ -1,10 +1,18 @@
+import { withFilter } from "graphql-subscriptions";
+import { client } from "../../client";
 import { NEW_MESSAGE } from "../../constants";
 import pubsub from "../../pubsub";
 
 const resolvers = {
     Subscription: {
         roomUpdates: {
-            subscribe: () => pubsub.asyncIterator(NEW_MESSAGE)
+            subscribe: withFilter(
+                () => pubsub.asyncIterator(NEW_MESSAGE),
+                (payload, variables) => {
+                    const { roomUpdates: { roomId }} = payload
+                    return roomId === variables.roomId;
+                }
+            )
         }
     }
 }
