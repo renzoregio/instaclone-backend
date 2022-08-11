8ce3f47362c36d3e8ccbb34909158821589a194f
renzoregio <renzorregio@gmail.com>
2022-08-11 16:52:37 +0800

    Filter subscriptions with existing rooms

--- src/messages/roomUpdates/roomUpdates.resolvers.ts
+++ src/messages/roomUpdates/roomUpdates.resolvers.ts
@@ -2,17 +2,27 @@ import { withFilter } from "graphql-subscriptions";
 import { client } from "../../client";
 import { NEW_MESSAGE } from "../../constants";
 import pubsub from "../../pubsub";
+import { Context } from "../../types/common/context";
 
 const resolvers = {
     Subscription: {
         roomUpdates: {
-            subscribe: withFilter(
-                () => pubsub.asyncIterator(NEW_MESSAGE),
-                (payload, variables) => {
-                    const { roomUpdates: { roomId }} = payload
-                    return roomId === variables.roomId;
+            subscribe: async(root, args: { roomId: number }, context, info) => {
+                const ok = await client.room.findUnique({ where: { id: args.roomId }, select: { id: true }})
+
+                if(!ok){
+                    throw new Error("Room not found.")
                 }
-            )
+
+                return withFilter(
+                    () => pubsub.asyncIterator(NEW_MESSAGE),
+                    (payload, variables) => {
+                        const { roomUpdates: { roomId }} = payload
+                        return roomId === variables.roomId;
+                    }
+                )(root, args, context, info);
+
+            }
         }
     }
 }
